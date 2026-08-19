beb7eb5f44630f6d424ef85be205ea9d1df31e9e
Long <vtv55hd@Gmail.com>
2026-08-19 07:53:33 +0700

    Add automation tests ORD-44 Manage Users

---
+++ automation-tests/tests/ORD-44_Manage Users/search-user-name_test.js
@@ -0,0 +1,21 @@
+Feature('ORD-73 - Search User Name');
+
+Scenario('Admin searches user by name', ({ I }) => {
+
+    I.amOnPage('/');
+
+    I.click('Đăng nhập');
+
+    I.wait(3);
+
+    I.fillField('input[type="email"]', 'manage@edulearn.vn');
+
+    I.fillField('input[type="password"]', 'admin123');
+
+    I.click('Đăng nhập');
+
+    I.wait(5);
+
+    I.saveScreenshot('ord73-search-user-name');
+
+});
